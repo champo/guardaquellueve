3531c5862a3d608fa320b7c93f71971786c412cc
champo <jcivile@gmail.com>
2010-02-13 18:44:19 -0300

    Fix to import fail

--- src/notify.py
+++ src/notify.py
@@ -5,6 +5,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
+import utils
 import tweepy
 
 from entities import Location, User
