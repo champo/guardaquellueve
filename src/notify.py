4251236c3cecb851898d56a55d54bd77c61eeed4
champo <jcivile@gmail.com>
2010-02-13 16:55:56 -0300

    Added support for daily notifications and now the actual locations that it's gonna rain in are selected.

--- src/notify.py
+++ src/notify.py
@@ -1,4 +1,5 @@
 import logging
+import datetime
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -7,24 +8,62 @@ import tweepy
 
 from entities import Location, User
 
+
 class RainNotification(webapp.RequestHandler):
 
-	def get(self):
-		#TODO: Magic method that tells where it's gonna rain in the next hour or two
+	def _format_message(location):
+		raise NotImplementedError()
 
-		rainy_places = Location.all()
+	def _get_locations():
+		raise NotImplementedError()
+
+	def get(self):
+
+		rainy_places = self._get_locations():
 		twitter = tweepy.API(auth_handler=tweepy.BasicAuthHandler('guardaquellueve', 'panchito123'))
 
 		for rainy_place in rainy_places:
 			users = User.all().filter('location =', rainy_place)
+			message = self._format_message(rainy_place)
+
 			for user in users:
-				twitter.send_direct_message(screen_name=user.screen_name, \
-						text="Guarda que llueve en una horita o dos")
+				twitter.send_direct_message(screen_name=user.screen_name, text=message)
+
+			#TODO: Mark this location as updated on the weather status!
+
+
+class HourlyNotification(RainNotification):
+
+	def _format_message(location):
+
+		#TODO: If we already told this place of rain in the previous update, then send a "Still raining update" instead
+
+		return "Va a llover en una horita o dos en %s" % (location.name, )
+
+	def _get_locations():
+		time_limit = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
+
+		return Location.all() \
+				.filter('next_rain_datetime <', time_limit) \
+				.filter('next_rain_datetime >', datetime.datetime.utcnow())
+
+
+class DailyNotification(RainNotification):
+
+	def _format_message(location):
+
+		return "Parece que mañana llueve en %s" % (location.name, )
+
+	def _get_locations():
+		min_time = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+
+		return Location.all().filter('next_rain_datetime >', min_time)
 
 def main():
 	logging.getLogger().setLevel(logging.DEBUG)
 	application = webapp.WSGIApplication([
-		('/cron/notify', RainNotification)
+		('/cron/notify/daily', DailyNotification),
+		('/cron/notify/hourly', HourlyNotification)
 		], debug=True)
 	run_wsgi_app(application)
 
