d88150d48fb5a9aa476e17169d577da1652d8453
Esteban Ordano <esteban@ordano.com.ar>
2010-02-13 15:41:11 -0300

    Entities and weather utils changed

--- src/entities.py
+++ src/entities.py
@@ -3,8 +3,8 @@ from google.appengine.ext import db
 class Location(db.Model):
 	station = db.StringProperty(required=True)
 	name = db.StringProperty(required=True)
+	prediction = db.StringProperty()
 
 class User(db.Model):
 	screen_name = db.StringProperty(required=True)
 	location = db.ReferenceProperty(Location)
-	prediction = db.StringProperty()
